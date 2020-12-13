65a0339f81c1821186cb9c7ba4704726315a6775
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2020-12-13 20:45:33 +0200

    -Add Activity Class -Add Migration of Activities -Seeding Dump data about Activities.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,33 +1,32 @@
-import React , { Component } from 'react';
-import './App.css';
-import { Header, Icon, List } from 'semantic-ui-react'
-import axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import { Header, Icon, List } from "semantic-ui-react";
+import axios from "axios";
 
-class App extends Component{
+class App extends Component {
   state = {
-    values : []
-  }
+    values: [],
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     //TODO:instead Localhost we use a real domainName
-    axios.get('http://localhost:5000/api/values')
-    .then((response) =>{
+    axios.get("http://localhost:5000/api/values").then((response) => {
       //console.log('response.data :>> ', response.data);
       this.setState({
-          values: response.data
+        values: response.data,
       });
     });
   }
 
-  render(){
+  render() {
     return (
       <div>
-        <Header as='h2'>
-          <Icon name='users' />
-          <Header.Content>Eventplus</Header.Content>
+        <Header as="h2">
+          <Icon name="users" />
+          <Header.Content>EventPlus</Header.Content>
         </Header>
         <List>
-          {this.state.values.map((value: any) =>(
+          {this.state.values.map((value: any) => (
             <List.Item key={value.id}>{value.name}</List.Item>
           ))}
         </List>
