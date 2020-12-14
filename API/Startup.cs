3c617a72d493035f4369b37ab1011cc0febb125e
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2020-12-14 21:47:15 +0200

    -Add Activities Controller -Add Get List of Activites Using MediatR Desgin Pattern -Add MediatR to Startup Services.

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -44,6 +46,9 @@ namespace API
                     .WithOrigins("http://localhost:3000");
                 });
             });
+
+            //except we have many handlers we need to tell services about only one
+            services.AddMediatR(typeof(List.Handler).Assembly);
             
             services.AddControllers();
 
