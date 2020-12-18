ed6b1a8dd21b07f2446d1703026738ded0ea15b2
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2020-12-18 22:00:43 +0200

    -Add EditMode property useState.

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -4,9 +4,10 @@ import { IActivity } from "../../../app/models/activity";
 
 interface IProps {
   activity: IActivity;
+  setEditMode: (editMode: boolean) => void;
 }
 
-const ActivityDetails: React.FC<IProps> = ({ activity }) => {
+const ActivityDetails: React.FC<IProps> = ({ activity , setEditMode }) => {
   return (
     <Card fluid>
       <Image
@@ -23,7 +24,7 @@ const ActivityDetails: React.FC<IProps> = ({ activity }) => {
       </Card.Content>
       <Card.Content extra>
         <Button.Group widths={2}>
-          <Button basic color="blue" content="Edit" />
+          <Button basic color="blue" content="Edit"  onClick={()=> setEditMode(true)}/>
           <Button basic color="grey" content="Cancel" />
         </Button.Group>
       </Card.Content>
