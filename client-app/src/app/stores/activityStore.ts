32305b307340ecdb4ab6773e0df01d1a3f6fcee8
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2020-12-29 15:29:31 +0200

    Using Async Await To refactoring loadActivities Function.

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -5,22 +5,26 @@ import agent from '../api/agent';
 
 class ActivityStore {
   @observable activities: IActivity[] = [];
-  @observable selectedActivity: IActivity |  undefined ;
+  @observable selectedActivity: IActivity | undefined;
   @observable loadingInitial = false;
   @observable editMode = false;
 
-  @action loadActivities = () => {
+  @action loadActivities = async () => {
     this.loadingInitial = true;
-    agent.Activities.list()
-      .then(response => {
-        response.forEach((activity) => {
-          activity.date = activity.date.split(".")[0];
-          this.activities.push(activity);
-        })
-      }).finally(() => this.loadingInitial = false);
+    try {
+      const activities = await agent.Activities.list();
+      activities.forEach((activity) => {
+        activity.date = activity.date.split(".")[0];
+        this.activities.push(activity);
+      });
+      this.loadingInitial = false;
+    } catch (error) {
+      console.log('error', error);
+      this.loadingInitial = false;
+    }
   }
 
-  @action selectActivity = (id: string) =>{
+  @action selectActivity = (id: string) => {
     this.selectedActivity = this.activities.find(a => a.id === id);
     this.editMode = false;
   }
